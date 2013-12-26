6095768f212647d511cde792fd17aa03dd389c90
root <root@ubuntu1.(none)>
2013-12-26 20:04:49 +0900

    update

--- utils/telnet_function.py
+++ utils/telnet_function.py
@@ -26,12 +26,12 @@ class Telnet_function:
    search_value = valid_values[0]
    for entry in self.tbentries_dict['switch']:
     result_msg = basic_msg+" where "+entry+"='"+search_value+"';"
-    print "this the result from command ("+result_msg+") ::"
+    print "this the result from command ("+result_msg+") ::\n"
     self.send_msg([self.database_name,result_msg,'echo'])
     print "\n"
   else:
    result_msg = basic_msg+";"
-   print "this the result from command ("+result_msg+") ::"
+   print "this the result from command ("+result_msg+") ::\n"
    self.send_msg([self.database_name,result_msg,'echo'])
    print "\n"
 
@@ -40,7 +40,8 @@ class Telnet_function:
   ### confirm the variables validation
   self.tbentries_dict['switch'].remove('id')
   self.tbentries_dict['switch_property'].remove('id')
-  required_count = len(self.tbentries_dict['switch'])+len(self.tbentries_dict['switch_property'])+1
+  self.tbentries_dict['switch_access'].remove('id')
+  required_count = len(self.tbentries_dict['switch'])+len(self.tbentries_dict['switch_property'])+len(self.tbentries_dict['switch_access'])+1
   valid_values = self.confirm_input_number(values,[0,required_count])
   ### get the information to register into the database from input or user-define
   if valid_values:
@@ -53,15 +54,17 @@ class Telnet_function:
     sys.exit()
   else:
    ### user define input processing
-   require_msgs={0:"1. What is the switch name? :",
-                 1:"2. What is the management ip address:port?(ex,10.0.0.1/24:23) :",
-                 2:"3. What group is this switch belonged to?[group name] :",
-                 3:"4. What is the vender name? :",
-                 4:"5. What is the product name? :",
-                 5:"6. What is the os version on? :",
-                 6:"7. How many port number does this switch have? :",
-                 7:"8. Where is the switch on? :",
-                 8:"9. Write the extra comment for description! :"}
+   require_msgs={0:" 1. What is the switch name? :",
+                 1:" 2. What is the management ip address:port?(ex,10.0.0.1/24:23) :",
+                 2:" 3. What group is this switch belonged to?[group name] :",
+                 3:" 4. What is the vender name? :",
+                 4:" 5. What is the product name? :",
+                 5:" 6. What is the os version on? :",
+                 6:" 7. How many port number does this switch have? :",
+                 7:" 8. Where is the switch on? :",
+                 8:" 9. What is the account to access? :",
+                 9:"10. What is the password to access? :",
+                10:"11. What is the enable password to access? :"}
    input_values=[]
    for require_msg in range(len(require_msgs.keys())):
     print require_msgs[require_msg],
@@ -75,7 +78,7 @@ class Telnet_function:
    ### input paramter validation check
    valid_params_list = list(self._register_paramter_validation_check(input_values))
 
-  ### check the valid parameter is already used or not
+  ### check the valid parameter is already used or not (using switch database)
   search_values = valid_params_list[0:2]
   for count in range(len(search_values)):
    search_values.reverse() 
@@ -93,8 +96,9 @@ class Telnet_function:
 
   ### register the data into the database
   self._insert_data_into_switch_table([switch_id]+valid_params_list[0:2])
-  self._insert_data_into_switch_property_table([switch_id]+valid_params_list[2:])
+  self._insert_data_into_switch_property_table([switch_id]+valid_params_list[2:9])
   self._insert_data_into_switch_status_table([switch_id,'deactive'])
+  self._insert_data_into_switch_access_table([switch_id]+valid_params_list[9:])
 
  def _insert_data_into_switch_table(self,values):
   uuid, name, ip = values
@@ -110,6 +114,12 @@ class Telnet_function:
   uuid,status = values
   sending_msg = "insert into switch_status (id,status) values ('"+uuid+"','"+status+"');"
   self.send_msg([self.database_name,sending_msg])
+
+ def _insert_data_into_switch_access_table(self,values):
+  uuid,account,password,enable = values
+  sending_msg = "insert into switch_access (id,account,password,enable) values ('"+uuid+"','"+account+"','"+password+"','"+enable+"');"
+  self.send_msg([self.database_name,sending_msg])
+  
  
  def _register_paramter_validation_check(self,values):
   switch_name = self.letter_status_confirm(values[0])
@@ -121,7 +131,10 @@ class Telnet_function:
   switch_port = self.int_number_format_confirm(values[6])
   switch_location = self.letter_status_confirm(values[7])
   switch_description = self.letter_status_confirm(values[8])
-  return switch_name,switch_ip,switch_group_name,switch_vendor,switch_product,switch_os_version,switch_port,switch_location,switch_description
+  switch_account = self.letter_status_confirm(values[9])
+  switch_password = values[10]
+  switch_enable = values[11]
+  return switch_name,switch_ip,switch_group_name,switch_vendor,switch_product,switch_os_version,switch_port,switch_location,switch_description,switch_account,switch_password,switch_enable
 
  ### removing the data from database with uuid
  def deregister_switch(self,values):
@@ -129,6 +142,7 @@ class Telnet_function:
   self._delete_data_from_switch_table(valid_values)
   self._delete_data_from_switch_property_table(valid_values)
   self._delete_data_from_switch_status_table(valid_values)
+  self._delete_data_from_switch_access_table(valid_values)
 
  def _delete_data_from_switch_table(self,values):
   sending_msg = "delete from switch where id='"+values[0]+"';"
@@ -140,4 +154,8 @@ class Telnet_function:
 
  def _delete_data_from_switch_status_table(self,values):
   sending_msg = "delete from switch_status where id='"+values[0]+"';"
+  self.send_msg([self.database_name,sending_msg])
+
+ def _delete_data_from_switch_access_table(self,values):
+  sending_msg = "delete from switch_access where id='"+values[0]+"';"
   self.send_msg([self.database_name,sending_msg])
